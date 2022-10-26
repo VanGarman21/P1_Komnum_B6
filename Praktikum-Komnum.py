bfc8c5e02c1c88a3f3bcbfeae1d48e6a89119862
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2022-10-26 19:13:23 +0700

    code+dokumentasi

--- Praktikum-Komnum.py
+++ Praktikum-Komnum.py
@@ -1,48 +1,61 @@
-import numpy as np 
-import pandas as pd
-import matplotlib.pyplot as plt
+# PROBLEM :
+# Dapatkan akar dari persamaan  f(x) = x^3 + x^2 – 3x – 3 = 0 yang terletak di antara x = 1 dan x = 2.
 
-def f(x):
-    return x**3 + x**2 - 3*x - 3 
+# ANALISIS :
+# Membuat sebuah program yang dapat menghitung akar dari persamaan f(x) = x^3 + x^2 – 3x – 3 = 0,
+# kemudian setelah ditemukan perubahan  tanda kita bisa mendapatkan interval baru.
 
-x1 = float(input("Masukkan Interval awal : "))
-x2 = float(input("Masukkan Interval akhir: "))
-n = int(input("Banyak Iterasi : "))
+import numpy as np # library numpy yang berfungsi untuk menggenerate angka
+import pandas as pd # library pandas untuk membantu dalam pembuatan tabel
+import matplotlib.pyplot as plt # library matplotlib untuk membantu dalam pembuatan grafik perkiraan
 
-start = x1
-end = x2
+def f(x): # mendefinisikan fungsi
+    return x**3 + x**2 - 3*x - 3 # mengembalikan nilai fungsi f(x) = x^3 + x^2 - 3x - 3 
 
-if f(x1)*f(x2) < 0:
-    ArrTable = []
-    for i in range(n):
-        x3 = (x1 + x2)/2
-        f1 = f(x1)
-        f2 = f(x2)
-        f3 = f(x3)
+x1 = float(input("Masukkan Interval awal : ")) # Perintah untuk memasukkan nilai interval awal
+x2 = float(input("Masukkan Interval akhir: ")) # Perintah untuk memasukkan nilai interval akhir
+n = int(input("Masukkan Banyak Iterasi : ")) # Perintah untuk memasukkan banyaknya iterasi
 
-        ArrTable.append([x1, x2, x3, f1, f2, f3])
+start = x1 # nilai interval awal untuk pembuatan grafik 
+end = x2 # nilai interval akhir untuk pembuatan grafik
 
-        if f1*f3 < 0:
-            x2 = x3
+if f(x1)*f(x2) < 0: # jika nilai 
+    tabel_tabulasi = [] # mendefinisikan array dengan nama "tabel_tabulasi"
+    for i in range(n): # melakukan perulangan sejumlah banyak iterasi (n)
+        
+        x3 = (x1 + x2)/2 # menghitung nilai x3
+        f1 = f(x1) # menghitung nilai f1, dengan memasukkan nilai x1 pada fungsi persamaan f(x)
+        f2 = f(x2) # menghitung nilai f2, dengan memasukkan nilai x2 pada fungsi persamaan f(x)
+        f3 = f(x3) # menghitung nilai f3, dengan memasukkan nilai x1 pada fungsi persamaan f(x)
 
-        elif f1*f3 > 0:
-            x1 = x3
+        tabel_tabulasi.append([x1, x2, x3, f1, f2, f3]) # memasukkan nilai x1, x2, x3, f1, f2,dan f3 kedalam array "tabel_tabulasi"
+      
+      # Dilakukan pengecekan untuk menentukan nilai x1 atau x2 berdasarkan nilai hasil perkalian f1*f3.  
+
+        if f1*f3 > 0: # jika nilai f1*f3 > 0, maka
+            x1 = x3 # nilai x1 = x3
             
-        else:
-            break
+        elif f1*f3 < 0: # jika nilai f1*f3 < 0, maka
+            x2 = x3 # nilai x2 = x3
+            
+        else: 
+            break # perulangan berhenti
+        
     print("-------------------------------------------------------------")
-    Table = pd.DataFrame(ArrTable, columns=['x1', 'x2', 'x3', 'f1', 'f2', 'f3'])
-    Table.index = np.arange(1, len(Table)+1)
-    print(Table)
+    tabel = pd.DataFrame(tabel_tabulasi, columns=['x1', 'x2', 'x3', 'f1', 'f2', 'f3']) # menambahkan nama kolom pada tabel
+    tabel.index = np.arange(1, len(tabel)+1) # memunculkan indeks iterasi ke-.. pada samping tabel
+    print(tabel) # mencetak tabel dengan isinya
     print("-------------------------------------------------------------")
-    print(f"\n Akar dari persamaan tersebut adalah {x3}\n")
-else:
-    print("Error Input")
+    
+    print(f"\n Akar dari persamaan tersebut adalah {x3}\n") # dioutputkan nilai x3, sebagai akar dari persamaan tersebut 
 
-x = np.arange(start, end, 0.01)
-y = f(x)
-plt.grid()
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.plot(x, y)
-plt.show()
+else:
+    print("Error Input") # mencetak "Error Input" ketika input yang dimasukkan tidak sesuai
+
+x = np.arange(start, end, 0.01) # mendeklarasikan variabel x = np.arange(start, end, 0.01)
+y = f(x) # mendeklarasikan variabel y = f(x)
+plt.grid() # menambahkan grid kedalam plot grafik
+plt.xlabel("X") # menambahkan xlabel "X" kedalam plot grafik
+plt.ylabel("Y") # menambahkan ylabel "Y" kedalam plot grafik
+plt.plot(x, y) # plot grafik x dan y
+plt.show() # menampilkan plot grafik 
